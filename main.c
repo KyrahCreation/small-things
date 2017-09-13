4849d8841ac688c09d0db4f75e6756199e7cdd42
Kyrah <contact@kyrah.fr>
2017-09-13 17:21:37 +0200

    Scripted update (no major change)

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for Christmas Sect (2nd edition) in /home/tom/Documents/Workspace/small-things
+** 
+** Made by Tom Partouche
+** Login   <tom@kyrah.fr>
+** 
+** Started on  Wed Sep 13 17:21:09 2017 Tom Partouche
+** Last update Wed Sep 13 17:21:18 2017 Tom Partouche
+*/
